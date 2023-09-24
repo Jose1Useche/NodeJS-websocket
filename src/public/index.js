1cd6644a6785a88f925cbc2c32707438b5ec2c95
Jose Useche <jose1useche@gmail.com>
2023-09-24 15:09:05 -0400

    Inicializando websocket

--- src/public/index.js
+++ src/public/index.js
@@ -1,3 +1,11 @@
-import express from "express";
+import app from "./app.js";
+import { Server as WebsocketServer } from "socket.io";
+import http from 'http';
 
-console.log('Hello world');
+const server = http.createServer(app); //Aunque parezca redundante, es necesario para la creacion de nuestro websocket.
+
+const httpServer = server.listen(3000,() => {
+    console.log('Server is up on port 3000.');
+});
+
+const io = new WebsocketServer(httpServer);
