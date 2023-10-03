240290fd173c47ac2c98649cc6a1bb013ccdbc26
Jose Useche <jose1useche@gmail.com>
2023-10-03 15:11:51 -0400

    Estableciendo nuestra conección entre front y back con websockets

--- src/db.js
+++ src/db.js
@@ -4,14 +4,14 @@ import { config } from "dotenv";
 config({ path: "./config/.env" })
 
 let uri = 
-        //   'mongodb://127.0.0.1:27017/bookstore';
-        process.env.MONGODB_CONNECT;
+          'mongodb://127.0.0.1:27017/bookstore';
+        // process.env.MONGODB_CONNECT;
 
 //connect to DB
 export const connectDB = async () => {
     try {
         await connect(uri);
-        return 'Connect to DB';
+        return 'Connected to DB';
     } catch (error) {
         throw new Error(error.message);
     }
--- src/index.js
+++ src/index.js
@@ -2,24 +2,23 @@ import app from "./app.js";
 import { Server as WebsocketServer } from "socket.io";
 import http from 'http';
 import { connectDB } from './db.js';
-import { response } from "express";
+import sockets from "./sockets.js";
 
-let httpServer = null;
-let io = null;
 const server = http.createServer(app); //Aunque parezca redundante, es necesario para la creacion de nuestro websocket.
+const io = new WebsocketServer(server);
 
 connectDB()
     .then(response => {
         console.log(response);
         
-        httpServer = server.listen(3000,() => {
+        server.listen(3000,() => {
             console.log('Server is up on port 3000.');
         });
         
-        io = new WebsocketServer(httpServer);
+        sockets(io);
     })
-    .catch(response => {
-        console.log(response);
+    .catch(error => {
+        console.log(error);
     });
 
 
